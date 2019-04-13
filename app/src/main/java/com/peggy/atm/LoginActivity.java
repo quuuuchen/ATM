687897d05556eb565a3671356749735609c6a5f7
quuuuchen <qucaerychen@gmail.com>
2019-04-13 13:53:36 +0800

    Initial commit

---
+++ app/src/main/java/com/peggy/atm/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.peggy.atm;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edUserid;
+    private EditText edUserpd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUserid = findViewById(R.id.uid);
+        edUserpd = findViewById(R.id.upd);
+
+    }
+
+    public void login(View view){
+        String userid = edUserid.getText().toString();
+        final String userpd = edUserpd.getText().toString();
+        FirebaseDatabase.getInstance().getReference("users").child(userid).child("password")
+                //getInstance取得資料庫物件，reference輸入第一層名稱，child(app輸入jack)取得jack的password
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {    //資料回傳時執行onDataChange
+                        String pw = (String) dataSnapshot.getValue();   //取得資料庫中user的password為pw
+                        if(pw.equals(userpd)){
+                            setResult(RESULT_OK);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+        //原本的方法，假的帳號密碼
+//        if("jack".equals(userid)&&"1234".equals(userpd)){
+//            setResult(RESULT_OK);   //只有登入成功時會回傳result_ok，供主畫面判斷是否正確登入
+//            finish();   //activity被呼叫時，會回到上一個activity
+//        }
+    }
+
+    public void quit(View view){
+
+    }
+}
---
+++ app/src/main/java/com/peggy/atm/MainActivity.java
@@ -0,0 +1,74 @@
+package com.peggy.atm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOGIN = 100;
+    boolean login = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!login){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent,REQUEST_LOGIN);
+            //startActivity(intent);
+        }
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==REQUEST_LOGIN){
+            if(requestCode!=RESULT_OK){  //loginActivity正確登入時才reqCode才會=result ok，不等於時就代表登入有問題，直接關閉
+                finish();
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
